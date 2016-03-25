c940de0d2d1416103547b5198cce01af481d5349
高見知英 <chie@onpu-tamago.net>
2016-03-25 13:43:07 +0900

    新規作成

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pico-hatena_counter
 Pico Plugins:はてなカウンターを埋め込むプラグイン
+
+はてなカウンターをページの最下部に自動挿入します。
+もし、Twig変数hatena_counterでカウンターを挿入するか、すでにはてなカウンター挿入コードがHTMLに含まれている場合、挿入しません。
+
+## 記事に追加する値
+
+なし
+
+##  追加するTwig変数
+
+ * hatena_counter:はてなカウンター挿入コード
+
+##  コンフィグオプション
+
+ * $config['hatenacounter']['name']:はてなユーザー名(TakamiChieなど)
+ * $config['hatenacounter']['id']:はてなカウンターのID
---
+++ pico_hatenacounter.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * はてなカウンター貼り付けプラグイン
+ *
+ * @author TakamiChie
+ */
+ 
+class Pico_HatenaCounter extends AbstractPicoPlugin {
+
+  protected $enabled = false;
+
+  private $counter_name = "";
+  
+  private $counter_id = 0;
+
+  private $code = "";
+  
+  public function onConfigLoaded(array &$config)
+  {
+    if(isset($config['hatenacounter'])){
+      $name = $config['hatenacounter']['name'];
+      $id = $config['hatenacounter']['id'];
+      $this->code = <<< CODE
+<script type="text/javascript"><!--
+    hatena_counter_name = "${name}";
+    hatena_counter_id = "${id}";
+    hatena_counter_ref = document.referrer+"";
+    hatena_counter_screen = screen.width + "x" + screen.height+","+screen.colorDepth;
+//--></script>
+<script type="text/javascript" src="http://counter.hatena.ne.jp/js/counter.js"></script>
+<noscript><img src="http://counter.hatena.ne.jp/TakamiChie/11" border="0" alt="counter"></noscript>
+
+CODE;
+
+    }
+  }
+
+  public function onPageRendering(Twig_Environment &$twig, array &$twigVariables, &$templateName)
+  {
+    if($this->code){
+      $twigVariables['hatena_counter'] = $this->code;
+    }
+  }
+
+  public function onPageRendered(&$output)
+  {
+    // 書き込み処理
+    if($this->code && strpos($output, "hatena_counter") === FALSE){
+      $output = preg_replace("/<\/body>/", "$this->code</body>", $output, 1);
+    }
+  }
+
+}
+
+?>
